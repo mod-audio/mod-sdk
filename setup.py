bd435dde78c22cd4e9012055c0e90f0465f87f4f
Luis Fagundes <lhfagundes@hacklab.com.br>
2014-04-22 18:20:34 -0300

    final changes for publishing modsdk. now you can install with "pip install modsdk"

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 import os, sys
 
+MANIFEST = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'MANIFEST.in')
+
 def data_dir(prefix, dirname):
     data_files = []
     html_files = []
@@ -8,16 +10,18 @@ def data_dir(prefix, dirname):
         fname = os.path.join(dirname, fname)
         if os.path.isfile(fname):
             html_files.append(fname)
+            open(MANIFEST, 'a').write('include %s\n' % fname)
         if os.path.isdir(fname):
             data_files += data_dir(prefix, fname)
     return [ (os.path.join(prefix, dirname), html_files) ] + data_files
 
+open(MANIFEST, 'w').write('include screenshot.js\n')
 share = os.path.join(sys.prefix, 'share')
 data_files = data_dir(share, 'html')
 data_files += [(share, ['screenshot.js'])]
 
 setup(name = 'modsdk',
-      version = '1.0',
+      version = '1.0.2',
       description = 'MOD plugin SDK.',
       author = "Luis Fagundes",
       author_email = "lhfagundes@hacklab.com.br",
@@ -36,6 +40,7 @@ setup(name = 'modsdk',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
         ],
+      include_package_data = True,
       url = 'http://github.com/portalmod/mod-sdk',
       
 )
