1dcad4d90999aae02806502cf688e913ca0fbd4c
Luis Fagundes <lhfagundes@hacklab.com.br>
2014-04-22 17:52:22 -0300

    version 1.0 now

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ share = os.path.join(sys.prefix, 'share')
 data_files = data_dir(share, 'html')
 data_files += [(share, ['screenshot.js'])]
 
-setup(name = 'mod-sdk',
-      version = '0.99.0',
+setup(name = 'modsdk',
+      version = '1.0',
       description = 'MOD plugin SDK.',
       author = "Luis Fagundes",
       author_email = "lhfagundes@hacklab.com.br",
