baa5f60e8c25e487c062a608f0b8d20397a82138
Luis Fagundes <lhfagundes@hacklab.com.br>
2014-06-10 09:33:16 -0300

    cache system can now be explicitly disabled

--- modsdk/cache.py
+++ modsdk/cache.py
@@ -4,69 +4,77 @@ from tornado import ioloop
 from modsdk.settings import UNITS_FILE
 from modcommon import lv2
 
+class WorkspaceCache(dict):
+    """
+    This dict is used to store the bundle data indexed by bundle name.
+    It uses pyinotify to monitor workspace directory and remove bundle cache
+    when any modification to that bundle is done in filesystem.
+    """
+
+    def __init__(self, basedir, enabled=True):
+        self.enabled = enabled
+
+        try:
+            import pyinotify
+        except ImportError:
+            self.enabled = False
+            return
+
+        self.basedir = os.path.realpath(basedir)
+        self.monitoring = False
+        self.cycle()
+
+    def monitor(self):
+        if not os.path.isdir(self.basedir) or self.monitoring:
+            return
+
+        self.monitoring = True
+
+        self.wm = pyinotify.WatchManager()
+        self.mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  | pyinotify.IN_CLOSE_WRITE
+
+        self.notifier = pyinotify.Notifier(self.wm, EventHandler(self), timeout=1)
+
+        self.register_dir(self.basedir)
+
+        super(WorkspaceCache, self).__init__()
+
+    def register_dir(self, path):
+        self.wm.add_watch(path, self.mask, rec=True)
+        try:
+            for filename in os.listdir(path):
+                filename = os.path.join(path, filename)
+                self.notify(filename)
+                if os.path.isdir(filename):
+                    self.register_dir(filename)
+        except OSError:
+            # Directory might have been removed
+            pass
+
+    def cycle(self):
+        if self.monitoring:
+            self.notifier.process_events()
+            if self.notifier.check_events():
+                self.notifier.read_events()
+            ioloop.IOLoop.instance().add_callback(self.cycle)
+        else:
+            for key in self.keys():
+                self.pop(key)
+            self.monitor()
+            # add_timeout instead of add_callback avoids eating a lot of cpu
+            # in an infinite loop
+            ioloop.IOLoop.instance().add_timeout(timedelta(milliseconds=1), self.cycle)
+
+
+    def notify(self, path):
+        path = path[len(self.basedir)+1:]
+        bundle = path.split('/')[0]
+        if self.get(bundle) is not None:
+            print "%s modified, clearing %s cache" % (path, bundle)
+            self.pop(bundle)
+
 try:
     import pyinotify
-
-    class WorkspaceCache(dict):
-        """
-        This dict is used to store the bundle data indexed by bundle name.
-        It uses pyinotify to monitor workspace directory and remove bundle cache
-        when any modification to that bundle is done in filesystem.
-        """
-        def __init__(self, basedir):
-            self.basedir = os.path.realpath(basedir)
-            self.monitoring = False
-            self.cycle()
-
-        def monitor(self):
-            if not os.path.isdir(self.basedir) or self.monitoring:
-                return
-
-            self.monitoring = True
-
-            self.wm = pyinotify.WatchManager()
-            self.mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  | pyinotify.IN_CLOSE_WRITE
-
-            self.notifier = pyinotify.Notifier(self.wm, EventHandler(self), timeout=1)
-
-            self.register_dir(self.basedir)
-
-            super(WorkspaceCache, self).__init__()
-
-        def register_dir(self, path):
-            self.wm.add_watch(path, self.mask, rec=True)
-            try:
-                for filename in os.listdir(path):
-                    filename = os.path.join(path, filename)
-                    self.notify(filename)
-                    if os.path.isdir(filename):
-                        self.register_dir(filename)
-            except OSError:
-                # Directory might have been removed
-                pass
-
-        def cycle(self):
-            if self.monitoring:
-                self.notifier.process_events()
-                if self.notifier.check_events():
-                    self.notifier.read_events()
-                ioloop.IOLoop.instance().add_callback(self.cycle)
-            else:
-                for key in self.keys():
-                    self.pop(key)
-                self.monitor()
-                # add_timeout instead of add_callback avoids eating a lot of cpu
-                # in an infinite loop
-                ioloop.IOLoop.instance().add_timeout(timedelta(milliseconds=1), self.cycle)
-
-
-        def notify(self, path):
-            path = path[len(self.basedir)+1:]
-            bundle = path.split('/')[0]
-            if self.get(bundle) is not None:
-                print "%s modified, clearing %s cache" % (path, bundle)
-                self.pop(bundle)
-
     class EventHandler(pyinotify.ProcessEvent):
 
         def __init__(self, monitor):
@@ -83,18 +91,8 @@ try:
 
         def process_IN_CLOSE_WRITE(self, event):
             self.monitor.notify(event.pathname)
-
 except ImportError:
-
-    class WorkspaceCache(dict):
-        def __init__(self, basedir):
-            super(WorkspaceCache, self).__init__()
-            self.cycle()
-
-        def cycle(self):
-            for key in self.keys():
-                self.pop(key)
-            ioloop.IOLoop.instance().add_timeout(timedelta(milliseconds=1), self.cycle)
+    pass
 
 BUNDLE_CACHE = None
 
@@ -105,14 +103,18 @@ def get_cache_instance(workspace):
         return BUNDLE_CACHE
     BUNDLE_CACHE = WorkspaceCache(workspace)
     return BUNDLE_CACHE
+
+def disable_cache(workspace):
+    get_cache_instance(workspace).enabled = False
     
 def get_bundle_data(workspace, bundle):
-    if BUNDLE_CACHE.get(bundle):
+    if BUNDLE_CACHE.enabled and BUNDLE_CACHE.get(bundle):
         return BUNDLE_CACHE[bundle]
     path = os.path.join(workspace, bundle)
     open(os.path.join(path, 'manifest.ttl'))
     package = lv2.Bundle(path, units_file=UNITS_FILE, allow_inconsistency=True)
-    BUNDLE_CACHE[bundle] = package.data
-    return BUNDLE_CACHE[bundle]
+    if BUNDLE_CACHE.enabled:
+        BUNDLE_CACHE[bundle] = package.data
+    return package.data
 
 
