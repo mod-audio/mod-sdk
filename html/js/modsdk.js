a0b27f25e7f4efd92771191063cf896d89d93394
falkTX <falktx@gmail.com>
2015-06-12 14:43:51 +0200

    Remove common.js file

--- html/js/modsdk.js
+++ html/js/modsdk.js
@@ -157,6 +157,30 @@ function loadBundles() {
 	   })
 }
 
+function getEffects(bundle, callback) {
+    window.location.hash = bundle
+    if (!bundle) {
+        effects.hide()
+        return
+    }
+    $.ajax({ url: '/effects',
+             data: {
+                 bundle: bundle,
+             },
+             success: function(result) {
+                 if (!result.ok) {
+                     alert(result.error)
+                     return
+                 }
+                 callback(result.data)
+             },
+             error: function(resp) {
+                 alert("Error: Can't get list of effects. Is your server running? Check the logs.")
+             },
+             dataType: 'json'
+        })
+}
+
 function loadEffects(callback) {
     var bundle = bundles.val()
     version.hide()
