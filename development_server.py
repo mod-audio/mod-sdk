b2b8f0f52735fded0e9251676ba4e9d73d36421f
falkTX <falktx@gmail.com>
2015-07-02 17:02:45 +0200

    Add initial style.css for gh-pages branch

--- development_server.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from modsdk.webserver import run
-
-if __name__ == "__main__":
-    run()
--- make_screenshot.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from modsdk.screenshot import run
-
-if __name__ == "__main__":
-    run()
