108d0d644492c5636190059ecfe91ae1e140c193
Markus Schmidt <schmidt@boomshop.net>
2015-10-05 20:25:03 +0200

    New lata backgrounds for generator

---
+++ html/resources/pedals/generator/lata.py
@@ -0,0 +1,58 @@
+options = { "gimp_image"    : "lata.xcf",
+            "css_source"    : "lata.css.in",
+            "css_dest"      : "../lata/lata.css",
+            "borderx"       : 0,
+            "bordery"       : 0,
+            "chdir"         : "../",
+            "color_prefix"  : "color_",
+            "mask_prefix"   : "mask_",
+            "export_suffix" : ".png" }
+
+colors =  { "black"         : "white",
+            "blue"          : "black",
+            "brown"         : "white",
+            "cream"         : "black",
+            "cyan"          : "black",
+            "darkblue"      : "white",
+            "gray"          : "black",
+            "green"         : "white",
+            "none"          : "white",
+            "orange"        : "black",
+            "petrol"        : "white",
+            "pink"          : "black",
+            "purple"        : "white",
+            "racing"        : "white",
+            "red"           : "white",
+            "white"         : "black",
+            "yellow"        : "black" }
+            
+sizes = ( { "x" : 396, "y" : 514, "folder" : "lata" }, )
+
+backgrounds_css = """.mod-pedal-lata{{{cns}}}.mod-<COLOR> { background-image:url(/resources/pedals/lata/<COLOR>.png{{{ns}}}); }
+"""
+
+colors_css = ( { "black" : """{
+    background-image:url(/resources/utils/dropdown-arrow-black.png{{{ns}}});
+}""",
+             "white" : """{
+    background-image:url(/resources/utils/dropdown-arrow-white.png{{{ns}}});
+}""",
+             "identifier" : """.mod-pedal-lata{{{cns}}}.mod-<COLOR> .mod-enumerated""" },
+             
+           { "black" : """{
+    color:black;
+}""",
+             "white" : """{
+    color:white;
+}""",
+             "identifier" : """.mod-pedal-lata{{{cns}}}.mod-<COLOR> h1,
+.mod-pedal-lata{{{cns}}}.mod-<COLOR> .mod-enumerated,
+.mod-pedal-lata{{{cns}}}.mod-<COLOR> .mod-control-group .mod-knob > span.mod-knob-title""" },
+            
+           { "black" : """{
+    border-color:black;
+}""",
+             "white" : """{
+    border-color:white;
+}""",
+             "identifier" : """.mod-pedal-lata{{{cns}}}.mod-<COLOR> h1""" } )
