491a49469df4eb324e605fdb94779e1addf214de
Markus Schmidt <schmidt@boomshop.net>
2015-10-16 03:42:24 +0200

    New knobs for british style

---
+++ html/resources/knobs/generator/configs/british.py
@@ -0,0 +1,11 @@
+options = { "type"          : "british",
+            
+            "gimp_image"    : "sources/british.xcf",
+            "blender_image" : "sources/british.blend",
+            "layers"        : [{ "scene" : "Knob",    "layer" : "knob",   "index" : 1000, "visible" : 1 },
+                               { "scene" : "Shadow",  "layer" : "shadow", "index" : 1000, "visible" : 1 }],
+            "color_prefix"  : "",
+            "chdir"         : "../british",
+            
+            "export_suffix" : ".png" }
+colors = { "british" : "none" }
