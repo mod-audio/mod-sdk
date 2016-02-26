f93220c4291b839a0ce6dff2ca7e7ee1378e3768
Markus Schmidt <schmidt@boomshop.net>
2016-02-26 13:59:50 +0100

    Add new chicken-head and mod-ui knobs including resource files

---
+++ html/resources/knobs/generator/configs/chickenhead.py
@@ -0,0 +1,8 @@
+options = { "type"          : "chickenhead",
+            
+            "blender_image" : "sources/chickenhead.blend",
+            "layers"        : [{ "scene" : "Knob",  "export" : "chicken-head" }],
+            
+            "chdir"         : "../chicken-head",
+            
+            "export_suffix" : ".png" }
---
+++ html/resources/knobs/generator/configs/mod-ui.py
@@ -0,0 +1,8 @@
+options = { "type"          : "mod-ui",
+            
+            "blender_image" : "sources/mod-ui.blend",
+            "layers"        : [{ "scene" : "Knob",  "export" : "mod-ui" }],
+            
+            "chdir"         : "../mod-ui",
+            
+            "export_suffix" : ".png" }
